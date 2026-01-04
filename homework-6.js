a62639a16ab18efdbda1b8d1171576d0b1689474
Kamran <kamran.shamshiyev.22@gmail.com>
2026-01-04 12:20:30 +0500

    renamed functions and variables and corrected comments

--- homework-6.js
+++ homework-6.js
@@ -1,23 +1,18 @@
-/* 3 - Создать объект на основе следующих данных: Имя, фамилия, 
-почта, работа, должность, возраст, страна, город, статус отношений 
-и так далее. Подобрать правильное название для переменной. */
+/* 3 - Создать объект на основе своих данных */
 
 const person = {
-  firstname: "Камран",
-  lastname: "Шамшиев",
+  firstname: "Kamran",
+  lastname: "Shamshiyev",
   age: 19,
   mail: "kamran.shamshiyev.22@gmail.com",
   job: "Developer",
   position: "Junior Frontend Developer",
-  country: "Россия",
-  city: "Новый Уренгой",
-  relationshipStatus: "холост",
+  country: "Russia",
+  city: "Novy Urengoy",
+  relationshipStatus: "single",
 }
 
-
-/* 4 - Создать объект, который будет хранить данные об автомобиле (марка, модель, год выпуска, цвет, вид коробки). 
-Добавить дополнительное свойство - владелец авто, значением которого будет ОБЪЕКТ, описанный в пункте №3. 
-Также добавлять отдельной строкой     car owner*/ 
+/* 4 - Создать объект с данными об автомобиле. Также добавить дополнительное свойство - владелец авто */ 
 
 const car = {
   carMake: "Hyundai",
@@ -25,96 +20,150 @@ const car = {
   modelYear: 2020,
   color: "Dusk Blue",
   transmissionType: "Automatic transmission",
-  carOwner: person
 }
 
+car.carOwner = person
+console.log(car)
 
-/* 5 - Написать функцию которая аргументом будет принимать объект, описанный в пункте №4. Она проверяет, 
-есть ли в объекте свойство "максимальная скорость", если нет - добавляет его и задает значение, если есть - 
-прекращает выполнение (ничего не делает) */
+/* 5 - Разработать функцию которая принимает объект и проверяет наличие свойства 
+"максимальная скорость", если нет - добавляет его и задает значение, если есть - прекращает выполнение  */
 
-const checkMaxSpeed = (car) => {
-  if ("maxSpeed" in car) {
-    console.log(`Максимальная скорость равна ${car.maxSpeed}`)
-    return
+const addMaxSpeed = (carInfo) => {
+  if ("maxSpeed" in carInfo) {
+    return;
   }
-  
-    car.maxSpeed = 240
-    console.log(`Максимальная скорость автомобиля ${car.maxSpeed}`)
+    carInfo.maxSpeed = 240
 }
 
-checkMaxSpeed(car)
-
+addMaxSpeed(car)
 
 /* 6 - Написать функцию, которая получает первым аргументом  — объект, 
 а вторым аргументом — свойство объекта, которое нужно вывести и выводит его значение. */
 
-const outputObject = (object, property) => {
-  console.log(object[property])
+const getAircraftInfo = (airplane, model) => {
+  console.log(airplane[model])
 }
 
-const user = {
-  name: "Kamran"
+const airplaneInfo = {
+  model: "Airbus 380-800"
 }
 
-outputObject(user, "name")
-
+getAircraftInfo(airplaneInfo, "model")
 
 /* 7 - Создать массив, который содержит названия продуктов (строки) */
 
 const products = [
-  "Молоко",
-  "Овсянка",
-  "Хлеб",
-  "Йогурт",
-  "Шоколад",
-  "Сыр"
-]
+  "milk",
+  "oatmeal",
+  "nuts",
+  "bread",
+  "chocolate",
+  "cheese"
+];
 
-
-/* 8 - Создать массив, состоящий из объектов, где объект представляет собой книгу 
-(название, автор, год выпуска, цвет обложки, жанр) (3-5 книг). После, используя известный метод массив, 
-добавить еще одну книгу в конец списка. */
+/* 8 - Создать массив, состоящий из объектов книг (или что-то своё), далее добавить еще один массив 
+в конец списка при помощи метода или оператора*/
  
-const setBooks = [
-  { name: "Атомные привычки", author: "Джеймс Клир", yearRelease: 2020, coverColor: "Белый", genre: "Личная эффективность"},
-  { name: "Этичный хакинг", author: "Юрген Эбнер", yearRelease: 2025, coverColor: "Темно-зеленый", genre: "Компьютерная литература"},
-  { name: "Война и мир", author: "Лев Толстой", yearRelease: 1868, coverColor: "бордовый", genre: "Роман-эпопея"},
-  { name: "Мидлмарч", author: "Джордж Элиот", yearRelease: 1871, coverColor: "синий", genre: "Роман"},
-  { name: "Унесенные ветром", author: "Маргарет Митчелл", yearRelease: 1936, coverColor: "бежевый", genre: "Драма"},
-]
-setBooks.push({ name: "Властелин колец. Братство кольца", author: "Джон Рональд Руэл Толкин", yearRelease: 1954, coverColor: "черный", genre: "Фэнтези"})
+const footballTeams = [
+  { 
+    teamName: "Manchester City", 
+    league: "Premier League",
+    dateFoundation: 1880,
+    clubPresident: "Khaldoon Al Mubarak",
+    headСoach: "Pep Guardiola", 
+    teamCaptain: "Bernardo Silva"
+  },
+  
+  { 
+    teamName: "Arsenal", 
+    league: "Premier League",
+    dateFoundation: 1886,
+    clubPresident: "Ken Friar",
+    headСoach: "Mikel Arteta", 
+    teamCaptain: "Martin Edegaard"
+  },
+  
+  { 
+    teamName: "Real Madrid", 
+    league: "Laliga",
+    dateFoundation: 1902,
+    clubPresident: "Florentino Perez",
+    headСoach: "Xabi Alonso", 
+    teamCaptain: "Dani Carvajal"
+  },
+  
+  { 
+    teamName: "Paris Saint-Germain", 
+    league: "Ligue 1",
+    dateFoundation: 1970,
+    clubPresident: "Nasser Al-Khelaifi",
+    headСoach: "Luis Enrique", 
+    teamCaptain: "Marquinhos"
+  },
+  
+  { 
+    teamName: "Al-Hilal",
+    league: "Saudi Pro League",
+    dateFoundation: 1957,
+    clubPresident: "Fahad bin Nafel",
+    headСoach: "Simone Inzaghi", 
+    teamCaptain: "Salim al-Dawsari"
+  },
+];
 
-console.log(setBooks)
+footballTeams.push(
+  { 
+    teamName: "Milan",
+    league: "Serie A",
+    dateFoundation: 1899,
+    clubPresident: "Paolo Scaroni",
+    headСoach: "Massimiliano Allegri", 
+    teamCaptain: "Mike Manyan"
+  }
+)
 
+console.log(footballTeams)
 
-/* 9 - Создать еще один массив, состоящих из тех же книг, но относящийся к определенной вселенной 
-(Гарри Поттер, Марвел и так далее). С помощью известного нам метода массива или оператора, объединить эти два массива в один */
+/* 9 - Создать еще один массив и соединить с массивом с предыдущего задания. С помощью метода массива или оператора, 
+объединить эти два массива в один */
 
-const booksFantasy = [
-  {name: "Властелин колец. Две крепости", author: "Джон Рональд Руэл Толкин", yearRelease: 1954, coverColor: "черный", genre: "Фэнтези"},
-  {name: "Властелин колец. Возвращение короля", author: "Джон Рональд Руэл Толкин", yearRelease: 1954, coverColor: "черный", genre: "Фэнтези"}
-]
+const seriaAClub = [
+  {
+    teamName: "Inter Milan",
+    league: "Serie A",
+    dateFoundation: 1908,
+    clubPresident: "Giuseppe Marotta",
+    headСoach: "Christian Chivu", 
+    teamCaptain: "Lautaro Martinez"
+  },
+  
+  {
+    teamName: "Juventus",
+    league: "Serie A",
+    dateFoundation: 1897,
+    clubPresident: "Gianluca Ferrero",
+    headСoach: "Luciano Spalletti", 
+    teamCaptain: "Manuel Locatelli"
+  }
+];
 
-const allBooks = [...setBooks, ...booksFantasy]
-console.log(allBooks)
+const allFootballTeams = [...footballTeams, ...seriaAClub]
+console.log(allFootballTeams)
 
+/* 10 - Написать функцию, которая принимает массив сущностей с предыдущего задания. 
+Далее добавить новое свойство "isRare" для объекта, которая будет принимать true или false в зависимости от года создания команды. */
 
-/* 10 - Написать функцию, которая принимает массив сущностей с задания №9. 
-Добавить новое свойство для объекта "isRare" и в зависимости от года выпуска книги (или какой-то логики, связанной с вашей сущностью), 
-установить true или false. Если книга выпущена позже 2000 года, устанавливаем true (да, это редкий), нет - false (значит это не редкий). */
-
- const isBookRare =  (rareBook) => {
-  return rareBook.map((allBooks)=> {
+ const isFootballTeamOld =  (oldClub) => {
+  return oldClub.map((allFootballTeams)=> {
     
-    if (allBooks.yearRelease > 2000) {
-      allBooks.isRare = true 
+    if (allFootballTeams.dateFoundation < 1900) {
+      allFootballTeams.isOld = true 
     } else {
-      allBooks.isRare = false
+      allFootballTeams.isOld = false
     }
     
-    return allBooks
+    return allFootballTeams
   })
  }
  
- isBookRare(allBooks) 
+ isFootballTeamOld(allFootballTeams) 
