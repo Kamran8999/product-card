a1ac5b344a7b4a52a5f31948679109cf5adccc37
Kamran <kamran.shamshiyev.22@gmail.com>
2026-01-17 18:05:42 +0500

    Changed the name and corrected some errors

--- homework-7.js
+++ homework-7.js
@@ -1,31 +1,31 @@
-import {userСomments} from "./comments.js"
+import {userСomments} from "./comments.js";
 
 /* 2 - Создать массив чисел от 1 до 10. Отфильтровать таким образом, чтобы мы получили массив из чисел начиная с 5. */
  
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const newArrayNumbers = numbers.filter(number => number >= [5])
+const newArrayNumbers = numbers.filter(number => number >= [5]);
 
 console.log(newArrayNumbers)
 
 /* 3 -  Создать массив строк любой сущности и проверить, есть ли в массиве какая-то определенная сущность. */
 
-const phoneManufacturers = ["Apple", "Samsung", "Xiaomi", "Honor", "Huawei", "Google Pixel", "Tecno", "Realme"]
+const phoneManufacturers = ["Apple", "Samsung", "Xiaomi", "Honor", "Huawei", "Google Pixel", "Tecno", "Realme"];
 
-const hasAppleManufacturer = phoneManufacturers.includes("Apple")
+const hasAppleManufacturer = phoneManufacturers.includes("Apple");
 
 console.log(hasAppleManufacturer)
 
 /* 4 - Написать функцию, которая принимает аргументом массив и ихменяет его порядок на противоположный. 
 Перевернуть две предыдущих массива. */
 
-const flipArray = (array) => {
+const reverseArray = (array) => {
   array.reverse()
   console.log(array)
-}
+};
 
-flipArray(newArrayNumbers)
-flipArray(phoneManufacturers)
+reverseArray(newArrayNumbers)
+reverseArray(phoneManufacturers)
 
 /* 5 - Добавить файл comments.js, в нём создать константу и в него поместить первые 10 объектов из массива по ссылке */
 
@@ -33,7 +33,7 @@ flipArray(phoneManufacturers)
 
 /* 7 - Вывести в консоль массив тех комментариев, почта пользователей которых содержит ".com". */
 
-const commentsWithComEmail = userСomments.filter(comment => comment.email.includes(".com"))
+const commentsWithComEmail = userСomments.filter(comment => comment.email.includes(".com"));
 
 console.log(commentsWithComEmail)
 
@@ -41,8 +41,9 @@ console.log(commentsWithComEmail)
 а те, у кого id > 5, имели postId: 1 */
 
 const commentsWithUpdatedPostId = userСomments.map(comment => {
-  return {...comment, postId: comment.id <= 5 ? 2 : 1}
-})
+  return {...comment, 
+    postId: comment.id <= 5 ? 2 : 1}
+});
 
 console.log(commentsWithUpdatedPostId)
 
@@ -57,7 +58,7 @@ console.log(commentsArrayName)
 
 const commentsWithValidation  = userСomments.map(comment => {
   return {...comment, isInvalid: comment.body.length > 180}
-})
+});
 
 console.log(commentsWithValidation)
 
@@ -66,7 +67,7 @@ console.log(commentsWithValidation)
 const userEmails = userСomments.reduce((emails, comment) => {
   emails.push(comment.email)
   return emails;
-}, [])
+}, []);
 
 console.log(userEmails)
 
@@ -78,8 +79,8 @@ console.log(emails)
 
 /* 12 - Перебрать массив с задания №11, привести его к строке. */
 
-const emailsAsString = emails.toString()
-const emailsString  = emails.join()
+const emailsAsString = emails.toString();
+const emailsString  = emails.join();
 
-console.log(emailsAsString )
+console.log(emailsAsString)
 console.log(emailsString)
