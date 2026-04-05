4f92e326b8619031dbd773960e4f3cfa20dd8648
Kamran <kamran.shamshiyev.22@gmail.com>
2026-04-06 00:44:34 +0500

    updated the main file

--- BusinessJet.js
+++ BusinessJet.js
@@ -13,9 +13,3 @@ export class BusinessJet extends Airliner {
   };
 };  
 
-const emiratesPlane = new Airliner('Emirates', 'Airbus', 'A380-800', 644);
-const saudiArabianPlane = new Airliner('Saudi Arabian', 'Boeing', '787 Dreamliner', 330);
-const netJetsPlane = new BusinessJet('NetJets', 'Cessna', 'Citation Latitude', 9, '16млн$');
-emiratesPlane.showInfoAirplane();
-saudiArabianPlane.showInfoAirplane();
-netJetsPlane.showInfoBusinessJet();
--- main.js
+++ main.js
@@ -5,5 +5,12 @@ import './homework-6.js';
 import './homework-7.js';
 import './homework-8.js';
 import './homework-9.js'; 
-import './Airliner.js';
-import './BusinessJet.js';
+import { Airliner } from './Airliner.js';
+import { BusinessJet } from './BusinessJet.js';
+
+const emiratesPlane = new Airliner('Emirates', 'Airbus', 'A380-800', 644);
+const saudiArabianPlane = new Airliner('Saudi Arabian', 'Boeing', '787 Dreamliner', 330);
+const netJetsPlane = new BusinessJet('NetJets', 'Cessna', 'Citation Latitude', 9, '16млн$');
+emiratesPlane.showInfoAirplane();
+saudiArabianPlane.showInfoAirplane();
+netJetsPlane.showInfoBusinessJet();
